dbaa432449cb5727fbabaca5fd9eaf0618c8ca83
dan933 <69749413+dan933@users.noreply.github.com>
2023-03-13 14:24:58 +1100

    project init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import navBar from './components/shared/navBar.vue';
+</script>
+
+<template>
+  <div class="app-container">
+    <navBar></navBar>
+    <main style="max-width:100vw; height: 100%; overflow: auto;">
+      
+    </main>
+  </div>
+</template>
+
+<style lang="scss">
+.app-container{
+  display: flex;
+  flex-flow: column;
+  height: 100vh;
+}
+
+</style>
---
+++ src/components/shared/navBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <nav
+    class="nav"
+    >
+        <v-toolbar
+        class="test"
+        title="Ange's Prototype"
+        color="primary"
+        >
+        </v-toolbar>
+        <v-icon icon="mdi-home" />
+    </nav>
+</template>
+
+<script lang="js">
+
+
+export default {
+    name: "navBar",
+}
+
+</script>
+
+<style lang="scss">
+.nav{
+    width: 100vw;
+}
+
+</style>
