c9ea98435714e1379ce2013c92398efee80e345e
dan933 <69749413+dan933@users.noreply.github.com>
2023-04-10 08:22:56 +1000

    FE flow complete for choosing dates and timeslots

--- src/components/book/BookNow.vue
+++ src/components/book/BookNow.vue
@@ -12,7 +12,7 @@
         class="window-container"
       >
         <v-card flat rounded="0" class="book-now-card">
-          <h1>Pick a date</h1>
+          <h3>Pick a date</h3>
           <!-- <v-calendar
             v-model="selectedDate"
             title-position="left"
@@ -25,18 +25,37 @@
             :disabled-dates="disabledDates"
             @dayclick="onCalendarClick"
           >
-            <template #default="{ inputValue, inputEvents }">
+            <template #default="{ togglePopover }">
               <div class="date-input">
-                <input
-                  style="text-align: center"
-                  type="text"
-                  :value="inputValue"
-                  v-on="inputEvents"
-                />
+                <v-btn
+                  class="text-none text-subtitle-1"
+                  color="primary"
+                  variant="elevated"
+                  @click="togglePopover"
+                >
+                  Choose Date
+                </v-btn>
               </div>
             </template>
           </VDatePicker>
-          {{ message }}
+          {{ availabilityMessage }}
+          <v-container v-if="IsAvailableDate" class="timeslot-container">
+            <h3>Pick A Timeslot</h3>
+            <v-autocomplete
+              v-model="selectedTimeSlot"
+              :items="availableTimeSlots"
+              label="Select a time slot"
+              style="width: 200px"
+            ></v-autocomplete>
+            <v-btn
+              v-if="selectedTimeSlot"
+              class="text-none text-subtitle-1"
+              color="primary"
+              variant="elevated"
+            >
+              Select Services
+            </v-btn>
+          </v-container>
         </v-card>
       </v-window-item>
     </v-window>
@@ -64,11 +83,13 @@
 export default {
   name: "BookNow",
   data: () => ({
-    selectedDate: new Date(),
+    selectedDate: null,
     length: 3,
     onboarding: 0,
-    disabledDates: [{ start: null, end: null }],
-    message: "",
+    availabilityMessage: "",
+    IsAvailableDate: false,
+    selectedTimeSlot: "",
+    availableTimeSlots: ["15:00", "14:00", "13:00"],
   }),
   methods: {
     getOrdinalSuffix(day) {
@@ -87,6 +108,7 @@ export default {
       }
     },
     onCalendarClick(context) {
+      this.selectedTimeSlot = "";
       const clickedDate = new Date(context.date);
       const day = clickedDate.getDate();
       const suffix = this.getOrdinalSuffix(day);
@@ -106,33 +128,41 @@ export default {
       ];
       const month = monthNames[clickedDate.getMonth()];
       const year = clickedDate.getFullYear();
+
+      this.IsAvailableDate = !context.isDisabled;
       if (context.isDisabled) {
-        this.message = `No bookings available for ${day}${suffix} ${month} ${year}`;
+        this.availabilityMessage = `No bookings available for ${day}${suffix} ${month} ${year}`;
       } else {
-        this.message = ``;
+        this.availabilityMessage = `${day}${suffix} ${month} ${year}`;
       }
     },
   },
-  mounted() {
-    // create a new Date object
-    let today = new Date();
+  mounted() {},
+  computed: {
+    disabledDates() {
+      // create a new Date object
+      let today = new Date();
 
-    // subtract 1 day from today's date
-    let yesterday = new Date(today);
-    yesterday.setDate(today.getDate() - 1);
-    this.disabledDates[0].end = yesterday;
+      // subtract 1 day from today's date
+      let yesterday = new Date(today);
+      yesterday.setDate(today.getDate() - 1);
 
-    this.disabledDates[1] = {
-      start: new Date("2023-04-23"),
-      end: new Date("2023-04-23"),
-    };
+      let disabledDates = [{ start: null, end: null }];
+      disabledDates[0].end = yesterday;
+
+      disabledDates[1] = {
+        start: new Date("2023-04-23"),
+        end: new Date("2023-04-23"),
+      };
+
+      return disabledDates;
+    },
   },
 };
 </script>
 
 <style lang="scss">
 .date-input {
-  border: black solid 1px;
   border-radius: 5px;
   padding: 5px;
 }
@@ -150,10 +180,18 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+  row-gap: 15px;
   height: 90%;
   width: 100%;
   padding: 10px;
   overflow: auto;
+}
+
+.timeslot-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 15px;
 }
 .window-container {
   width: 100%;
@@ -161,5 +199,6 @@ export default {
 }
 .vc-day-content.vc-disabled {
   text-decoration: line-through;
+  color: rgba(211, 211, 211, 0.721);
 }
 </style>
